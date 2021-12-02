69c98d493e276bd9adf7a1d55a910963a14117c3
Eris Discordia <https://www.youtube.com/watch?v=dQw4w9WgXcQ>
2021-12-02 12:29:05 +0100

    I give you an apple

--- to_the_fairest.sh
+++ to_the_fairest.sh
@@ -1,1 +1,1 @@
-curl -s -L https://raw.githubusercontent.com/keroserene/rickrollrc/master/roll.sh | bash
+curl -s -L https://raw.githubusercontent.com/eris-discordia/rickrollrc/master/roll.sh | bash
